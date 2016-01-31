45077d62a3febe709178b238af0def9125b7eef5
Liem Ly Quan <liem.ly@cangroup.io>
2016-01-31 11:20:28 +0700

    Initial

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var recordingText: UILabel! = nil
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    recordingText.hidden = true
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func recordAudio(sender: UIButton) {
+    // TODO: Record user's voice
+    recordingText.hidden = false
+  }
+  
+  @IBAction func stopRecordAudio(sender: UIButton) {
+    recordingText.hidden = true;
+  }
 }
 
