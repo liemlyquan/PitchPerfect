1ff9241b85f49a72f44f160c83162ece8f8cec55
Liem Ly Quan <liem.ly@cangroup.io>
2016-02-18 19:31:55 +0700

    Finish and add README

--- PitchPerfect/PlaySoundViewController.swift
+++ PitchPerfect/PlaySoundViewController.swift
@@ -12,17 +12,15 @@ import AVFoundation
 class PlaySoundViewController: UIViewController {
   var audioPlayer:AVAudioPlayer!
   var receivedAudio:RecordedAudio!
+  var audioEngine:AVAudioEngine!
+  var audioFile:AVAudioFile!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-//    if let filePath = NSBundle.mainBundle().URLForResource("movie_quote", withExtension: "mp3") {
-//
-//    } else {
-//      print("Invalid file path")
-//    }
-    
     audioPlayer = try! AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
     audioPlayer.enableRate = true
+    audioEngine = AVAudioEngine()
+    audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
   }
 
   override func didReceiveMemoryWarning() {
@@ -39,5 +37,32 @@ class PlaySoundViewController: UIViewController {
     audioPlayer.stop()
     audioPlayer.rate = 2
     audioPlayer.play()
+  }
+  @IBAction func playChipmunkAudio(sender: UIButton) {
+    playAudioWithVariablePitch(1000)
+  }
+  @IBAction func playDarthvaderAudio(sender: UIButton) {
+    playAudioWithVariablePitch(-1000)
+  }
+  
+  func playAudioWithVariablePitch(pitch: Float){
+    audioPlayer.stop()
+    audioEngine.stop()
+    audioEngine.reset()
+    
+    let audioPlayerNode = AVAudioPlayerNode()
+    audioEngine.attachNode(audioPlayerNode)
+    
+    let changePitchEffect = AVAudioUnitTimePitch()
+    changePitchEffect.pitch = pitch
+    audioEngine.attachNode(changePitchEffect)
+    
+    audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+    audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+    
+    audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+    try! audioEngine.start()
+    
+    audioPlayerNode.play()
   }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# PitchPerfect
+
+A simple Swift program (for learning) to record and manipulate voice based on Intro To iOS App Development with Swift on Udacity
